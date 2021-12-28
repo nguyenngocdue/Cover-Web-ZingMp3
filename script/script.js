a122fd5ea80c33bdcb725a67c34861631222ecd6
nguyenngocdue <68887000+nguyenngocdue@users.noreply.github.com>
2021-12-28 23:28:40 +0700

    #Zing Chart

--- script/script.js
+++ script/script.js
@@ -6,6 +6,9 @@ const navThemeBtn = $('.header__nav-btn.nav-btn--theme');
 const modalTheme = $(' .modal__theme');
 const closeModalBtn = $(' .modal__close-btn')
 
+const songLists = Array.from($$('.playlist__list-song'));
+
+
 
 
 //Open and close model theme
@@ -25,3 +28,40 @@ modalTheme.onclick = (e) => {
 closeModalBtn.onclick = (e) => {
     modalTheme.classList.remove('open')
 }
+
+
+
+//Handle when click on icons heart
+songLists.forEach(songList => {
+    songList.onclick = function(e) {
+        const heartIconBtn = e.target.closest('.song-btn--heart');
+        const micIconBtn = e.target.closest('.btn--mic')
+        // console.log(heartIconBtn, "Parents")
+
+
+        //Handle when click on icons heart
+        if (heartIconBtn) {
+            const heartIcon =heartIconBtn.firstElementChild;
+            // console.log(heartIcon, "Child")
+
+            if(heartIcon.classList.contains('primary')){
+                heartIcon.classList.replace('bi-heart-fill', 'bi-heart');
+            }
+            else {
+                heartIcon.classList.replace('bi-heart', 'bi-heart-fill');
+                heartIcon.classList.toggle('primary');
+            }
+        }
+
+        ////Handle when click on icons miccro
+        if(micIconBtn) {
+            const micIcon = micIconBtn.firstElementChild
+            micIcon.classList.toggle('primary');
+    
+        }
+
+
+
+    }
+});
+
